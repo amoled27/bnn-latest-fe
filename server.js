6ee1dfc2ff204ec969b5f441f284f21eff80f62d
amoled27 <amodshinde1995@gmail.com>
2019-05-26 17:34:15 +0530

    first deploy1236

--- server.js
+++ server.js
@@ -24,12 +24,12 @@ const forceSSL = function () {
 app.use(forceSSL());
 // Run the app by serving the static files
 // in the dist directory
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/bnn-angular'));
 // Start the app by listening on the default
 // Heroku port
 // For all GET requests, send back index.html
 // so that PathLocationStrategy can be used
 app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/bnn-angular/index.html'));
 });
 app.listen(process.env.PORT || 8080);
